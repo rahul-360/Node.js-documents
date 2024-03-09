a08229c46d218ac6bd202509c57b41e0bd86d917
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-03-09 22:19:49 +0530

    node file module

---
+++ hello-world/file.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+// sync
+// fs.writeFileSync("./text.txt", "Hey Node.js 1.0");
+
+
+// Async
+// fs.writeFile("./text.txt", "Hey Node.js 2.0", (err) => { });
+
+
+// read from contacts.txt 
+
+// const result = fs.readFileSync("./contacts.txt", "utf-8")
+// console.log(result);
+
+
+// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+//     if (err) {
+//         console.log("Error", err);
+//     } else {
+//         console.log("Result", result);
+//     }
+// })
+
+// fs.appendFileSync("./text.txt", new Date().getDate().toLocaleString());
+fs.appendFileSync("./text.txt", `${Date.now()} Hey Node.js\n`);
--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,1 +1,6 @@
 console.log("Hello World Node.Js V2.0");
+
+const math = require("./math");
+console.log(math); 
+console.log(math.addfun(2, 10));
+console.log(math.subfun(20, 10));
