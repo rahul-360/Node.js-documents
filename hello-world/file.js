a6c5f3692922b01895d45eb27b4e641d05d67e9c
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-03-10 23:00:00 +0530

    file update

--- hello-world/file.js
+++ hello-world/file.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 // fs.writeFileSync("./text.txt", "Hey Node.js 1.0");
 
 
-// Async
-// fs.writeFile("./text.txt", "Hey Node.js 2.0", (err) => { });
+Async
+fs.writeFile("./text.txt", "Hey Node.js 2.0", (err) => { });
 
 
 // read from contacts.txt 
@@ -14,13 +14,13 @@ const fs = require("fs");
 // console.log(result);
 
 
-// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
-//     if (err) {
-//         console.log("Error", err);
-//     } else {
-//         console.log("Result", result);
-//     }
-// })
+fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("Result", result);
+    }
+})
 
 // fs.appendFileSync("./text.txt", new Date().getDate().toLocaleString());
 fs.appendFileSync("./text.txt", `${Date.now()} Hey Node.js\n`);
